a233bc9a2fe68e07d2f66ec431f10df2e562c3ba
jackphillips1994 <jhphillips1994@gmail.com>
2017-06-29 12:51:57 +0000

    Created DB and DB interaction functions

---
+++ model.py
@@ -0,0 +1,50 @@
+# Python file that contains the sensor class
+# More than temperature sensors can be added to this program
+
+from peewee import *
+
+# Defining the database
+db = SqliteDatabase('reptile.db', check_same_thread=False)
+
+# Classes to handle talking to the DB Tables
+class Sensor(Model):
+    name = CharField()
+    device_id = CharField()
+    device_type = CharField()
+    pin = CharField()
+
+    class Meta:
+        database = db
+
+class SensorReading(Model):
+    time = DateTimeField()
+    name = CharField()
+    value = FloatField()
+
+    class Meta:
+        database = db
+
+
+class SensorData(object):
+
+    def __init__(self):
+        db.connect()
+        db.create_tables([Sensor, SensorReading], safe=True)
+
+    def define_sensor(self, name, device_type, device_id, pin):
+        Sensor.get_or_create(name=name, device_type=device_type, device_id=device_id, pin=pin)
+
+    def get_sensors(self):
+        return Sensor.select()
+
+    def get_recent_readings(self, name, limit=30):
+        return SensorReading.select() \
+                            .where(SensorReading.name == name) \
+                            .order_by(SensorReading.time.desc()) \
+                            .limit(limit)
+
+    def add_reading(self, time, name, value):
+        SensorReading.create(time=time, name=name, value=value)
+
+    def close(self):
+        db.close()
